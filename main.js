79f82e7b0164bfd2c1daf9c7bb17ed850eb80e4f
alberto__segura <alberto.segura.delgado@gmail.com>
2015-11-18 19:44:04 +0100

    se añaden servicios REST y tests para ellos

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ var usuarios = db.get("usuarios");
 var Empresa = new (require('./lib/Empresa')).EmpresaControlador(empresas);
 var Usuario = new (require('./lib/Usuario')).UsuarioControlador(usuarios);
 
+/* Pruebas...
 Empresa.add("primera", function(err, result){
 	if(err){
 		console.log(err);
@@ -36,12 +37,6 @@ Empresa.add("primera", function(err, result){
 			}
 			console.log(result);
 
-			/*
-			Empresa.list(3, function(err, result){
-				console.log(result);
-			});
-			*/
-
 			
 			Empresa.addCalificacionByName(result[0]._id, "primera", 2, Usuario, function(err, result){
 				if(err){
@@ -56,6 +51,61 @@ Empresa.add("primera", function(err, result){
 		});
 	});
 });
+
+*/
+
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.put('/empresa/:nombre', function( req, response ) {
+	// creamos la nueva empresa en la BD
+	Empresa.add(req.params.nombre, function(err, r){
+		if(err)
+		{
+			response.status(500).send(err);
+		}
+		else
+		{
+			response.send(r);
+		}
+	});
+});
+
+app.delete('/empresa/:nombre', function( req, response ) {
+	// creamos la nueva empresa en la BD
+	Empresa.delete(req.params.nombre, function(err, r){
+		if(err || !r)
+		{
+			response.status(500).send(err);
+		}
+		else
+		{
+			response.send({"code":0});
+		}
+	});
+});
+
+app.get('/empresas', function(request, response) {
+	Empresa.list(100, function(err, r){
+		if(err)
+		{
+			response.status(500).send(err);
+		}
+		else
+		{
+			response.send(r);
+		}
+	});
+});
+
+app.listen(PORT, function() {
+  console.log("Node app is running at localhost:" + PORT);
+});
+
+
+module.exports = app;
+
 
 /*
 var Empresa = require("./lib/Empresa.js");
--- test/test.js
+++ test/test.js
@@ -7,23 +7,27 @@ var db = monk('localhost:27017/p1ej2');
 var empresas = db.get("empresas");
 var usuarios = db.get("usuarios");
 
-var Empresa = new (require('./lib/Empresa')).EmpresaControlador(empresas);
-var Usuario = new (require('./lib/Usuario')).UsuarioControlador(usuarios);
+var Empresa = new (require('../lib/Empresa')).EmpresaControlador(empresas);
+var Usuario = new (require('../lib/Usuario')).UsuarioControlador(usuarios);
 
+var request = require('supertest'), 
+    app = require('../main.js');
 
 describe('Empresa', function() {
   describe('Crea', function () {
-    it('Debe crear la empresa correctamente', function () {
+    it('Debe crear la empresa correctamente', function (done) {
     	Empresa.add("empresatest", function(err, result){
     		assert.equal(result.nombre, "empresatest", "Creada");
+        done();
     	});
     });
   });
 
   describe('Elimina', function () {
-    it('Debe eliminar al usuario correctamente', function () {
-    	Empresa.delete("usuariotest", function(err){
+    it('Debe eliminar al usuario correctamente', function (done) {
+    	Empresa.delete("empresatest", function(err){
     		assert.equal(err, undefined, "Eliminada");
+        done();
     	});
     });
   });
@@ -31,18 +35,38 @@ describe('Empresa', function() {
 
 describe('Usuario', function() {
   describe('Crea', function () {
-    it('Debe crear al usuario correctamente', function () {
+    it('Debe crear al usuario correctamente', function (done) {
     	Usuario.add("usuariotest", function(err, result){
     		assert.equal(result.nombreusuario, "usuariotest", "Creado");
+        done();
     	});
     });
   });
 
   describe('Elimina', function () {
-    it('Debe eliminar al usuario correctamente', function () {
+    it('Debe eliminar al usuario correctamente', function (done) {
     	Usuario.delete("usuariotest", function(err){
     		assert.equal(err, undefined, "Eliminado");
+        done();
     	});
     });
   });
 });
+
+describe('Peticiones', function() {
+  describe('PUT', function () {
+    it('Crear Empresa', function (done) {
+        request(app)
+          .put('/empresa/empresadeprueba')
+          .expect(200,done);
+    });
+  });
+  describe('DELETE', function () {
+    it('Borrar Empresa', function (done) {
+        request(app)
+          .delete('/empresa/empresadeprueba')
+          .expect(200, done);
+    });
+  });
+});
+
